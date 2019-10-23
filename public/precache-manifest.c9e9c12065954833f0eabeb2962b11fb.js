c5975b2f5134b24256ad211ea949ace8c739de5a
opimand <xwhiskasx@gmail.com>
2019-10-23 10:01:42 +0300

    Done app

---
+++ public/precache-manifest.c9e9c12065954833f0eabeb2962b11fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956f83753f6e111f63579042d8bfc4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ed36ae6ae34538d604",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3f924b7c120d8205f554",
+    "url": "/static/js/2.42c1282c.chunk.js"
+  },
+  {
+    "revision": "15ed36ae6ae34538d604",
+    "url": "/static/js/main.34f00d93.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
